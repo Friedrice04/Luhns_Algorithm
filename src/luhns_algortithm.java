6603e2439f838eb5859b73b55d23ab983b2b256f
Friedrice04 <hdowd12@gmail.com>
2022-10-10 23:20:01 +0100

    luhn

--- src/luhns_algortithm.java
+++ src/luhns_algortithm.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class luhns_algortithm {
+import java.util.*;
+public class luhns_algortithm {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        for(int i=0; i<num.length(); i++){
+            int n=num.charAt(i) - '0';
+            numList.add(n);
+        }
+        int start=0;
+        if(numList.size()%2==0){
+            start=1;
+        }
+        for(int i=start; i<numList.size(); i=i+2){
+            int n=numList.get(i)*2;
+            if(n>9){
+                numList.set(i, (n-9));
+            }else{
+                numList.set(i, n);
+            }
+            int sum=0;
+            for(int j=0; i<numList.size(); j++){
+                sum= sum+numList.get(j);
+            }
+            int ans=0;
+            if(sum%10!=0){
+                ans =  10-(sum%10);
+            }
+            System.out.println(ans);
+        }
+    }
 }
